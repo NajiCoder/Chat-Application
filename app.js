0cc6672d75b1aedca5adb5c79403932c0d8f27d6
Naji <lemiine987@gmail.com>
2023-03-28 16:37:00 +0300

    Finshed layout and started implementing JQuerry

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
 const express = require("express");
+
+// Create the app
+const app = express();
+
+// Set up EJS
+app.set('view engine', 'ejs');
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+    res.render("home");
+});
+
+
+app.listen(3000, () => {
+    console.log("Servere listening on por 3000");
+})
---
+++ public/js/home.js
@@ -0,0 +1,10 @@
+// js scrips
+$(".submit-btn").click(function(){ 
+    let data = {
+        name: $("#name").val(),
+        roomId: $("#roomId").val()
+    };
+    console.log(data);
+});
+
+// Paused at 1:02:11
